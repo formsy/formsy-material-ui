1f56c02164ab99b35db2dd40f43e48491faa3052
Matt Brookes <matt@brookes.net>
2015-11-01 23:02:21 +0000

    Revert "Changes in v0.2.3 was a mistake. This commit fixes that by using the …"

--- formsy-material-ui.jsx
+++ formsy-material-ui.jsx
@@ -100,56 +100,15 @@ let FormsySelect = React.createClass({
 });
 
 let FormsyText = React.createClass({
-  displayName: 'FormsyText',
-
-  propTypes: {
-    name: React.PropTypes.string.isRequired,
-    validations: React.PropTypes.string,
-    validationError: React.PropTypes.string,
-    hintText: React.PropTypes.string,
-    floatingLabelText: React.PropTypes.string
-  },
-
-  handleChange: function handleChange(event) {
-    if(this.getErrorMessage() != null){
-      this.setValue(event.currentTarget.value);
-    }
-    else{
-      if (this.isValidValue(event.target.value)) {
-        this.setValue(event.target.value);
-      }
-      else{
-        this.setState({
-          _value: event.currentTarget.value,
-          _isPristine: false
-        });
-      }
-    }
-  },
-
-  handleValueChange: function handleValueChange(event, value) {
-    this.setValue(value);
-  },
-
-  handleBlur: function handleBlur(event) {
-    this.setValue(event.currentTarget.value);
-  },
-
-  handleEnterKeyDown: function handleEnterKeyDown(event) {
-    this.setValue(event.currentTarget.value);
-  },
-
-  mixins: [ Formsy.Mixin ],
+  mixins: [ Formsy.Mixin, FormComponentMixin ],
 
   render: function () {
     return (
       <TextField
         {...this.props}
-        onChange: this.handleChange,
-        onBlur: this.handleBlur,
-        onEnterKeyDown:this.handleEnterKeyDown,
-        errorText: this.getErrorMessage(),
-        value: this.getValue() } />
+        onBlur={this.handleChange}
+        errorText={this.getErrorMessage()}
+        value={this.getValue()} />
     );
   }
 });
