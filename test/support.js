bd39a981545ba651f684f65bcc2e1c0f275a1122
Ryan Wise <ryanmwise1@gmail.com>
2016-02-08 13:06:23 -0800

    set up unit-testing tools
    Also write a few unit tests to get things started.

---
+++ test/support.js
@@ -0,0 +1,17 @@
+import { mount } from 'enzyme'
+import { Form } from 'formsy-react'
+
+export const mountTestForm = (childrenFn, formProps={}) => {
+  class TestForm extends React.Component {
+    render () {
+      return (
+        <Form {...this.props}>
+          {childrenFn()}
+        </Form>
+      )
+    }
+  }
+
+  const formWrapper = mount(<TestForm {...formProps} />)
+  return formWrapper
+}
---
+++ test/test.utils.js
@@ -0,0 +1,60 @@
+import FormsyText from '../src/FormsyText'
+import TextField from 'material-ui/lib/text-field'
+import FormsyRadioGroup from '../src/FormsyRadioGroup'
+import RadioButtonGroup from 'material-ui/lib/radio-button-group'
+import { mountTestForm } from './support'
+
+const setup = () => {
+  const formWrapper = mountTestForm(childrenFn)
+  const formsyTextWrapper = formWrapper.find(FormsyText)
+  const formsyRadioGroupWrapper = formWrapper.find(FormsyRadioGroup)
+  return {
+    formsyTextInstance: formsyTextWrapper.node,
+    textFieldInstance: formsyTextWrapper.find(TextField).node,
+    formsyRadioGroupInstance: formsyRadioGroupWrapper.node,
+    toggleInstance: formsyRadioGroupWrapper.find(RadioButtonGroup).node,
+  }
+}
+
+const childrenFn = () => (
+  <div>
+    <FormsyText name="text" />
+    <FormsyRadioGroup name="radio" />
+  </div>
+)
+
+describe('_setMuiComponentAndMaybeFocus', () => {
+  it('exposes a`focus` method if the underlying material-ui component does', () => {
+    const {
+      formsyTextInstance,
+      textFieldInstance,
+      formsyRadioGroupInstance,
+      toggleInstance,
+    } = setup()
+
+    expect(formsyTextInstance.focus).to.be.a('function')
+    expect(textFieldInstance.focus).to.be.a('function')
+
+    expect(formsyRadioGroupInstance.focus).to.be.undefined
+    expect(toggleInstance.focus).to.be.undefined
+  })
+
+  describe('the exposed `focus` method', () => {
+    it('calls `focus` on the material-ui component', () => {
+      const {
+        formsyTextInstance,
+        textFieldInstance,
+      } = setup()
+
+      textFieldInstance.focus = sinon.spy(textFieldInstance.focus)
+
+      expect(textFieldInstance.focus).to.not.have.been.called
+
+      formsyTextInstance.focus()
+      expect(textFieldInstance.focus).to.have.been.calledOnce
+
+      formsyTextInstance.focus()
+      expect(textFieldInstance.focus).to.have.been.calledTwice
+    })
+  })
+})
