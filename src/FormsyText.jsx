cf19f38b1dec2083d5e476216cb1efb174cb5f1b
mbrookes <github@nospam.33m.co>
2016-04-17 22:27:26 +0100

    [FormsyText] Uncontrolled use for better performance

--- src/FormsyText.jsx
+++ src/FormsyText.jsx
@@ -14,26 +14,6 @@ let FormsyText = React.createClass({
     onBlur: React.PropTypes.func
   },
 
-  handleChange: function handleChange(event) {
-    if(this.getErrorMessage() != null){
-      this.setValue(event.currentTarget.value);
-      if (this.props.onChange) this.props.onChange(event, event.currentTarget.value);
-    }
-    else{
-      if (this.isValidValue(event.target.value)) {
-        this.setValue(event.target.value);
-        if (this.props.onChange) this.props.onChange(event, event.target.value);
-      }
-      else{
-        this.setState({
-          _value: event.currentTarget.value,
-          _isPristine: false
-        });
-        if (this.props.onChange) this.props.onChange(event, event.currentTarget.value);
-      }
-    }
-  },
-
   handleBlur: function handleBlur(event) {
     this.setValue(event.currentTarget.value);
     if (this.props.onBlur) this.props.onBlur(event);
@@ -44,11 +24,6 @@ let FormsyText = React.createClass({
     if (this.props.onKeyDown) this.props.onKeyDown(event, event.currentTarget.value);
   },
 
-  handleEnterKeyDown: function handleEnterKeyDown(event) {
-    this.setValue(event.currentTarget.value);
-    if (this.props.onEnterKeyDown) this.props.onEnterKeyDown(event, event.currentTarget.value);
-  },
-
   _setMuiComponentAndMaybeFocus: _setMuiComponentAndMaybeFocus,
 
   render: function () {
@@ -57,12 +32,10 @@ let FormsyText = React.createClass({
         {...this.props}
         ref={this._setMuiComponentAndMaybeFocus}
         defaultValue={this.props.value}
-        onChange={this.handleChange}
         onBlur={this.handleBlur}
         onFocus={this.props.onFocus}
         onKeyDown={this.handleKeyDown}
         errorText={this.getErrorMessage()}
-        value={this.getValue()}
       />
     );
   }
