2c69bf180755163d7237d0086f99ce59e9ea04cf
mbrookes <github@nospam.33m.co>
2016-04-17 22:28:03 +0100

    [Tests] Update MUI paths

--- test/index.js
+++ test/index.js
@@ -4,11 +4,11 @@ import sinonChai from 'sinon-chai'
 import chaiEnzyme from 'chai-enzyme'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
-window.React = React
-window.expect = chai.expect
-chai.use(sinonChai)
-chai.use(chaiEnzyme())
-injectTapEventPlugin()
+window.React = React;
+window.expect = chai.expect;
+chai.use(sinonChai);
+chai.use(chaiEnzyme());
+injectTapEventPlugin();
 
-const tests = require.context('.', true, /test\..+\.js$/)
-tests.keys().forEach(tests)
+const tests = require.context('.', true, /test\..+\.js$/);
+tests.keys().forEach(tests);
